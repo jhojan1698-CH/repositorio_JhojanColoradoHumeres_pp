0d3b82f61751610f6c30476f043ef396b85b8ef2
Jhojan <Jhojan@PC-Jhojan>
2025-09-24 23:19:17 -0500

    Carga Inicial

---
+++ src/main/java/programas/Programa_pp.java
@@ -0,0 +1,35 @@
+
+package programas;
+
+import java.util.Scanner;
+
+public class Programa_pp {
+       public static void main (String[] args) {
+        
+        //declarar variables
+        String empleado;
+        double ht, th, sb, des, sn;
+        Scanner lectura = new Scanner(System.in);
+        
+        //entradas
+        System.out.print("Nombre del Empleado: ");
+        empleado=lectura.next();
+        
+        System.out.print("Horas Trabajadas: ");
+        ht=lectura.nextDouble();
+        
+        System.out.print("Tarifa por Hora: ");
+        th=lectura.nextDouble();
+        
+        //proceso de datos
+        sb=ht*th;
+        des=sb*0.13;
+        sn=sb-des;
+        
+        //salida de datos
+        System.out.println("Sueldo Bruto: "+sb);
+        System.out.println("Descuento: "+des);
+        System.out.println("Sueldo Neto: "+sn);
+        
+    }
+}
